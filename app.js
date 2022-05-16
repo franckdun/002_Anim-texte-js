a503b329dd0ca45a0b387de730d6b10fa3864e57
franckdun <scorven7@gmail.com>
2022-05-16 12:49:07 +0200

    premier ajout

---
+++ app.js
@@ -0,0 +1,31 @@
+const txtAnim = document.querySelector('h1');
+
+console.log(new Typewriter(txtAnim));
+
+new Typewriter(txtAnim, {
+    loop: true,
+    // deleteSpeed: 20
+})
+.changeDelay(50)
+.typeString('Moi c\'est John Doe')
+.pauseFor(300)
+.typeString('<strong>, Dev Full-Stack</strong> !')
+.pauseFor(3000)
+.deleteChars(13)
+.pauseFor(300)
+.typeString('<strong>eloppeur ? </strong>')
+
+.pauseFor(1000)
+.deleteChars(11)
+.typeString('<span style="color: #27ae60;"> CSS</span> !')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color: midnightblue;"> React</span> !')
+.pauseFor(1000)
+.deleteChars(7)
+.typeString('<span style="color: #ea39ff;"> PhP</span> !')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color: #ff6910;"> JavaScript</span> !')
+.pauseFor(1000)
+.start()
